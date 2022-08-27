8fe80fc87c2f265d41d7f27fbfd866313fce3ef9
Hector Almonte <hbalmontess272@gmail.com>
2022-08-27 16:51:46 -0300

    Agregar archivos de proyecto.

---
+++ ChatSignalR/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            if (!string.IsNullOrEmpty(message) && !string.IsNullOrEmpty(user))
+            {
+                await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+            }
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho",
+                $"Alguien se conectó {Context.ConnectionId}");
+        }
+    }
+}
---
+++ ChatSignalR/Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSignalR.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms =
+            new Dictionary<int, string>()
+            {
+                {1, "Programación" },
+                {2, "Multimedia" },
+                {3, "Energias Renovables" },
+                {4, "Sonido" }
+            };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
